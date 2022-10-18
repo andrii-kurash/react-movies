ff30698c9f266296e80aee077fd77a3d0fbf7e4b
Andrii <kurash.andrii@gmail.com>
2022-10-18 17:21:50 +0300

    add links to repo

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
       <div className="footer-copyright">
         <div className="container">
           © 2022 Copyright Text
-          <a className="grey-text text-lighten-4 right" href="#!">
+          <a className="grey-text text-lighten-4 right" href="https://github.com/andrii-kurash/react-movies" target="_blank" rel="noreferrer">
             Repo
           </a>
         </div>
